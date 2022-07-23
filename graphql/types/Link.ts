878b9df3aeeef431a9f0030c9bbe4a3d46001a54
putcho01 <53213591+putcho01@users.noreply.github.com>
2022-07-23 15:21:35 +0900

    Add: CreateLink機能 :tada:

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -1,4 +1,4 @@
-import { objectType, extendType, intArg, stringArg } from 'nexus'
+import { nonNull, objectType, stringArg, intArg, extendType } from 'nexus';
 import { User } from './User'
 
 export const Link = objectType({
@@ -116,4 +116,47 @@ export const LinksQuery = extendType({
       },
     })
   },
-})
+});
+
+export const CreateLinkMutation = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('createLink', {
+      type: Link,
+      args: {
+        title: nonNull(stringArg()),
+        url: nonNull(stringArg()),
+        imageUrl: nonNull(stringArg()),
+        category: nonNull(stringArg()),
+        description: nonNull(stringArg()),
+      },
+      async resolve(_parent, args, ctx) {
+        if (!ctx.user) {
+          throw new Error(`You need to be logged in to perform an action`)
+        }
+
+        const user = await ctx.prisma.user.findUnique({
+          where: {
+            email: ctx.user.email,
+          },
+        });
+
+         if (user.role !== 'ADMIN') {
+          throw new Error(`You do not have permission to perform action`);
+        }
+
+        const newLink = {
+          title: args.title,
+          url: args.url,
+          imageUrl: args.imageUrl,
+          category: args.category,
+          description: args.description,
+        };
+
+        return await ctx.prisma.link.create({
+          data: newLink,
+        });
+      },
+    });
+  },
+});
